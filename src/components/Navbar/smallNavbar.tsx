fcb14b28c806a0cb88616819dbae69a82ec0114a
abh31000 <abdelkhalekbouklihacene67@gmail.com>
2023-05-03 14:05:16 +0100

    initial small navbar

--- src/components/Navbar/smallNavbar.tsx
+++ src/components/Navbar/smallNavbar.tsx
@@ -1,7 +1,40 @@
+import { useEffect, useState } from "react"
+
+function MenuButton({handlestate}:any){
+    return(
+        <svg onClick={handlestate} className="my-auto cursor-pointer relative p-1 border-black bord box-content left-4" width="52" height="13" viewBox="0 0 28 5" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M1 0H18" stroke="black" stroke-width="1.4" stroke-linecap="round"/>
+                    <path d="M1 5H14" stroke="black" stroke-width="1.4" stroke-linecap="round"/>
+        </svg>
+    )
+}
+
+function Menu(){
+    return (
+        <div className="relative font-extrabold font-[Guminert] select-none h-72 px-10 bg-white">
+            <h1 className="text-[60px]" ><a href="#">Posts</a></h1>
+            <h1 className="text-[60px]"><a href="#">Newletter</a></h1>
+        </div>
+    )
+}
+
 export default function SmallNavbar(){
+    const [open, setOpen] = useState(false)
+
+    
+    function handleMenu(){
+        if(open === false) return setOpen(true)
+        else return setOpen(false) 
+        
+    }
+
     return(
         <>
-            <h1>S nav</h1>
+            <div className="flex justify-between select-none px-10 h-24 bg-white">
+                <h1 className="select-none my-auto text-[36px] font-[Nokwy]"><a href="#"> BlOg<span className="text-transparent  stroke-black-1">osphere</span></a></h1>
+                <MenuButton handlestate={handleMenu}></MenuButton>
+            </div>
+            {open ? <Menu></Menu>: ""}
         </>
     )
 }
