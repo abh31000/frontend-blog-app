c61bf304056b37dcfe39dce0b0b2e2fb23e682b6
abh31000 <abdelkhalekbouklihacene67@gmail.com>
2023-05-03 01:40:40 +0100

    large navbar

--- src/components/Navbar/largeNavbar.tsx
+++ src/components/Navbar/largeNavbar.tsx
@@ -1,7 +1,14 @@
 export default function LargeNavbar(){
     return(
         <>
-            <h1>L nav</h1>
+            <div className="flex justify-between px-16 h-24 bg-white">
+                <h1 className="select-none my-auto text-[40px] font-[Nokwy]"><a href="#"> BlOg<span className="text-transparent stroke-black-1">osphere</span></a></h1>
+                <div className="flex select-none my-auto space-x-11 font-extrabold font-[Guminert] ">
+                    <h1 className="text-[64px]" ><a href="#">Posts</a></h1>
+                    <h1 className="text-[64px]"><a href="#">Newletter</a></h1>
+                </div>
+                
+            </div>
         </>
     )
 }
