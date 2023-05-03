42a1e25c7461989b8c4185f5d8dbb4abfa987aa7
abh31000 <abdelkhalekbouklihacene67@gmail.com>
2023-05-03 13:14:44 +0100

    Working menu button for medium navbar

--- src/components/Navbar/mediumNavbar.tsx
+++ src/components/Navbar/mediumNavbar.tsx
@@ -1,15 +1,41 @@
-export default function MediumNavbar(){
+import { useEffect, useState } from "react"
+
+function MenuButton({handlestate}:any){
     return(
-        <>
-            <div className="flex h-24 select-none px-12 w-full justify-between bg-white">
-                <h1 className="select-none my-auto text-[40px] font-[Nokwy]"><a href="#"> BlOg<span className="text-transparent  stroke-black-1">osphere</span></a></h1>
-                <svg className="my-auto cursor-pointer relative p-1 border-black bord box-content left-3" width="56" height="13" viewBox="0 0 28 5" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <svg onClick={handlestate} className="my-auto cursor-pointer relative p-1 border-black bord box-content left-3" width="56" height="13" viewBox="0 0 28 5" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path d="M1 0H21" stroke="black" stroke-width="1.4" stroke-linecap="round"/>
                     <path d="M1 5H17" stroke="black" stroke-width="1.4" stroke-linecap="round"/>
-                </svg>
+        </svg>
+    )
+}
+
+function Menu(){
+    return (
+        <div className="relative font-extrabold font-[Guminert] select-none h-72 px-16 bg-white">
+            <h1 className="text-[64px]" ><a href="#">Posts</a></h1>
+            <h1 className="text-[64px]"><a href="#">Newletter</a></h1>
+        </div>
+    )
+}
 
 
+export default function MediumNavbar(){
+    const [open, setOpen] = useState(false)
+
+    
+    function handleMenu(){
+        if(open === false) return setOpen(true)
+        else return setOpen(false) 
+        
+    }
+    
+    return(
+        <>
+            <div className="flex h-24 select-none px-16 w-full justify-between bg-white">
+                <h1 className="select-none my-auto text-[40px] font-[Nokwy]"><a href="#"> BlOg<span className="text-transparent  stroke-black-1">osphere</span></a></h1>
+                <MenuButton handlestate={handleMenu}></MenuButton>
             </div>
+            {open ? <Menu></Menu>: ""}
         </>
     )
 }
